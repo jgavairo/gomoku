63d4c2d5501c77322958baa4ec58f7c1b87ed732
jgavairo <jordan.gavairon@gmail.com>
2025-10-02 14:11:56 +0200

    refact: begining cleaned code, new function for init button.

--- src/scene/Settings.cpp
+++ src/scene/Settings.cpp
@@ -3,74 +3,25 @@
 #include "audio/Volumes.hpp"
 #include "util/Preferences.hpp"
 
+constexpr const char* ON_KEY = "sound_on";
+constexpr const char* OFF_KEY = "sound_off";
+
 namespace gomoku::scene {
 
 SettingsScene::SettingsScene(Context& ctx)
     : AScene(ctx)
 {
-    // Boutons de thème
-    defaultBtn_.setPosition({ 1020, 580 });
-    defaultBtn_.setSize({ 300, 70 });
-    defaultBtn_.setScale(0.2f);
-    defaultBtn_.setCallback([this]() { applyTheme("default"); });
-    defaultBtn_.setHoverCallback([this]() { playSfx("ui_hover", UI_HOVER_VOLUME); });
-    if (context_.resourceManager && context_.resourceManager->hasTexture("default_theme_button"))
-        defaultBtn_.setTexture(&context_.resourceManager->getTexture("default_theme_button"));
-    
-    halloweenBtn_.setPosition({ 1150, 580 });
-    halloweenBtn_.setSize({ 300, 70 });
-    halloweenBtn_.setScale(0.2f);
-    halloweenBtn_.setCallback([this]() { applyTheme("halloween"); });
-    halloweenBtn_.setHoverCallback([this]() { playSfx("ui_hover", UI_HOVER_VOLUME); });
-    if (context_.resourceManager && context_.resourceManager->hasTexture("halloween_theme_button"))
-        halloweenBtn_.setTexture(&context_.resourceManager->getTexture("halloween_theme_button"));
-
-    pastelBtn_.setPosition({ 1280, 580 });
-    pastelBtn_.setSize({ 300, 70 });
-    pastelBtn_.setScale(0.2f);
-    pastelBtn_.setCallback([this]() { applyTheme("pastel"); });
-    pastelBtn_.setHoverCallback([this]() { playSfx("ui_hover", UI_HOVER_VOLUME); });
-    if (context_.resourceManager && context_.resourceManager->hasTexture("pastel_theme_button"))
-        pastelBtn_.setTexture(&context_.resourceManager->getTexture("pastel_theme_button"));
-
-    // Bouton retour
-    backBtn_.setPosition({ 695, 730 });
-    backBtn_.setSize({ 300, 70 });
-    if (context_.resourceManager && context_.resourceManager->hasTexture("back_button"))
-        backBtn_.setTexture(&context_.resourceManager->getTexture("back_button"));
-    backBtn_.setScale(1.0f);
-    backBtn_.setCallback([this]() { onBackClicked(); });
-    backBtn_.setHoverCallback([this]() { playSfx("ui_hover", UI_HOVER_VOLUME); });
-
-    // Bouton SFX toggle
-    sfxToggleBtn_.setPosition({ 1150, 340 });
-    sfxToggleBtn_.setSize({ 10, 10 });
-    sfxToggleBtn_.setScale(0.15f);
-    sfxToggleBtn_.setCallback([this]() { toggleSfx(); });
-    sfxToggleBtn_.setHoverCallback([this]() { playSfx("ui_hover", UI_HOVER_VOLUME); });
-
-    // Bouton Musique toggle
-    musicToggleBtn_.setPosition({ 1150, 460 });
-    musicToggleBtn_.setSize({ 10, 10 });
-    musicToggleBtn_.setScale(0.15f);
-    musicToggleBtn_.setCallback([this]() { toggleMusic(); });
-    musicToggleBtn_.setHoverCallback([this]() { playSfx("ui_hover", UI_HOVER_VOLUME); });
-
-    // Texture initiale selon état
-    if (context_.resourceManager) {
-        const char* onKey = "sound_on";
-        const char* offKey = "sound_off";
-        if (context_.resourceManager->hasTexture(context_.sfxEnabled ? onKey : offKey))
-            sfxToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.sfxEnabled ? onKey : offKey));
-        if (context_.resourceManager->hasTexture(context_.musicEnabled ? onKey : offKey))
-            musicToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.musicEnabled ? onKey : offKey));
-    }
+    initButtons(defaultBtn_, "default_theme_button", { 1020, 580 }, 0.2f, [this]() { applyTheme("default"); });
+    initButtons(halloweenBtn_, "halloween_theme_button", { 1150, 580 }, 0.2f, [this]() { applyTheme("halloween"); });
+    initButtons(pastelBtn_, "pastel_theme_button", { 1280, 580 }, 0.2f, [this]() { applyTheme("pastel"); });
+    initButtons(backBtn_, "back_button", { 695, 730 }, 1.0f, [this]() { onBackClicked(); });
+    initButtons(sfxToggleBtn_, "sound_on", { 1150, 340 }, 0.15f, [this]() { toggleSfx(); });
+    initButtons(musicToggleBtn_, "sound_off", { 1150, 460 }, 0.15f, [this]() { toggleMusic(); });
 }
 
 void SettingsScene::update(sf::Time& deltaTime)
 {
     defaultBtn_.update(deltaTime);
-    darkBtn_.update(deltaTime);
     halloweenBtn_.update(deltaTime);
     pastelBtn_.update(deltaTime);
     backBtn_.update(deltaTime);
@@ -81,7 +32,6 @@ void SettingsScene::update(sf::Time& deltaTime)
 void SettingsScene::render(sf::RenderTarget& target) const
 {
     defaultBtn_.render(target);
-    darkBtn_.render(target);
     halloweenBtn_.render(target);
     pastelBtn_.render(target);
     backBtn_.render(target);
@@ -103,18 +53,15 @@ void SettingsScene::onThemeChanged()
     if (context_.resourceManager->hasTexture("pastel_theme_button"))
         pastelBtn_.setTexture(&context_.resourceManager->getTexture("pastel_theme_button"));
     // rafraîchit icônes (thémées)
-    const char* onKey = "sound_on";
-    const char* offKey = "sound_off";
-    if (context_.resourceManager->hasTexture(context_.sfxEnabled ? onKey : offKey))
-        sfxToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.sfxEnabled ? onKey : offKey));
-    if (context_.resourceManager->hasTexture(context_.musicEnabled ? onKey : offKey))
-        musicToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.musicEnabled ? onKey : offKey));
+    if (context_.resourceManager->hasTexture(context_.sfxEnabled ? ON_KEY : OFF_KEY))
+        sfxToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.sfxEnabled ? ON_KEY : OFF_KEY));
+    if (context_.resourceManager->hasTexture(context_.musicEnabled ? ON_KEY : OFF_KEY))
+        musicToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.musicEnabled ? ON_KEY : OFF_KEY));
 }
 
 bool SettingsScene::handleInput(sf::Event& event)
 {
     if (context_.window && defaultBtn_.handleInput(event, *context_.window)) { playSfx("ui_click", BUTTON_VOLUME); return true; }
-    if (context_.window && darkBtn_.handleInput(event, *context_.window)) { playSfx("ui_click", BUTTON_VOLUME); return true; }
     if (context_.window && halloweenBtn_.handleInput(event, *context_.window)) { playSfx("ui_click", BUTTON_VOLUME); return true; }
     if (context_.window && pastelBtn_.handleInput(event, *context_.window)) { playSfx("ui_click", BUTTON_VOLUME); return true; }
     if (context_.window && backBtn_.handleInput(event, *context_.window)) { playSfx("ui_click", BUTTON_VOLUME); return true; }
@@ -163,10 +110,8 @@ void SettingsScene::toggleSfx()
         prefs.musicEnabled = context_.musicEnabled;
         gomoku::util::Preferences::save(prefs);
     }
-    const char* onKey = "sound_on";
-    const char* offKey = "sound_off";
-    if (context_.resourceManager && context_.resourceManager->hasTexture(context_.sfxEnabled ? onKey : offKey))
-        sfxToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.sfxEnabled ? onKey : offKey));
+    if (context_.resourceManager && context_.resourceManager->hasTexture(context_.sfxEnabled ? ON_KEY : OFF_KEY))
+        sfxToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.sfxEnabled ? ON_KEY : OFF_KEY));
 }
 
 void SettingsScene::toggleMusic()
@@ -188,10 +133,8 @@ void SettingsScene::toggleMusic()
         prefs.musicEnabled = context_.musicEnabled;
         gomoku::util::Preferences::save(prefs);
     }
-    const char* onKey = "sound_on";
-    const char* offKey = "sound_off";
-    if (context_.resourceManager && context_.resourceManager->hasTexture(context_.musicEnabled ? onKey : offKey))
-        musicToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.musicEnabled ? onKey : offKey));
+    if (context_.resourceManager && context_.resourceManager->hasTexture(context_.musicEnabled ? ON_KEY : OFF_KEY))
+        musicToggleBtn_.setTexture(&context_.resourceManager->getTexture(context_.musicEnabled ? ON_KEY : OFF_KEY));
 }
 
 } // namespace gomoku::scene
